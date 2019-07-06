d19db770ffc00b7db082cea4a050e6056ca81432
Raphael Ram <ramraphael@gmail.com>
2019-07-06 19:43:39 -0400

    Intial config

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import App from './App';
+
+const container = document.getElementById('app');
+
+reactDom.render(<App/>, container);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {  
+  devServer: {
+    compress: true,
+    port: 3000
+  },
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ["babel-plugin-styled-components"]
+          }
+        }
+      }     
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.join(__dirname, '/client/index.html')
+  })]
+}
